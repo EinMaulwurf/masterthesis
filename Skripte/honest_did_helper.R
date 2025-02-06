f8936e6ec2feb51bbb409ff2831aad3d843b7d1a
EinMaulwurf <geis.sebi@gmail.com>
2025-02-06 10:31:20 +0100

    Verwende honestDiD helper function direkt aus dem HonestDiD git repo

--- Skripte/honest_did_helper.R
+++ Skripte/honest_did_helper.R
@@ -1,7 +1,11 @@
+# Taken from https://github.com/asheshrambachan/HonestDiD/blob/master/R/honest_did.R
+
 #' @title honest_did
 #'
 #' @description a function to compute a sensitivity analysis
 #'  using the approach of Rambachan and Roth (2021)
+#'
+#' @param ... Parameters to pass to the relevant method.
 honest_did <- function(...) UseMethod("honest_did")
 
 #' @title honest_did.AGGTEobj
@@ -10,6 +14,7 @@ honest_did <- function(...) UseMethod("honest_did")
 #'  using the approach of Rambachan and Roth (2021) when
 #'  the event study is estimating using the `did` package
 #'
+#' @param es Result from aggte (object of class AGGTEobj).
 #' @param e event time to compute the sensitivity analysis for.
 #'  The default value is `e=0` corresponding to the "on impact"
 #'  effect of participating in the treatment.
@@ -18,8 +23,11 @@ honest_did <- function(...) UseMethod("honest_did")
 #'  in pre-treatment periods) or "relative_magnitude" (which
 #'  conducts a sensitivity analysis based on the relative magnitudes
 #'  of deviations from parallel trends in pre-treatment periods).
-#' @inheritParams HonestDiD::createSensitivityResults
-#' @inheritParams HonestDid::createSensitivityResults_relativeMagnitudes
+#' @param gridPoints Number of grid points used for the underlying test
+#'  inversion. Default equals 100. User may wish to change the number of grid
+#'  points for computational reasons.
+#' @param ... Parameters to pass to `createSensitivityResults` or
+#'  `createSensitivityResults_relativeMagnitudes`.
 honest_did.AGGTEobj <- function(es,
                                 e          = 0,
                                 type       = c("smoothness", "relative_magnitude"),
@@ -45,14 +53,39 @@ honest_did.AGGTEobj <- function(es,
   n <- nrow(es_inf_func)
   V <- t(es_inf_func) %*% es_inf_func / n / n
   
+  # Check time vector is consecutive with referencePeriod = -1
+  referencePeriod <- -1
+  consecutivePre  <- !all(diff(es$egt[es$egt <= referencePeriod]) == 1)
+  consecutivePost <- !all(diff(es$egt[es$egt >= referencePeriod]) == 1)
+  if ( consecutivePre | consecutivePost ) {
+    msg <- "honest_did expects a time vector with consecutive time periods;"
+    msg <- paste(msg, "please re-code your event study and interpret the results accordingly.", sep="\n")
+    stop(msg)
+  }
+  
   # Remove the coefficient normalized to zero
-  referencePeriodIndex <- which(es$egt == -1)
-  V    <- V[-referencePeriodIndex,-referencePeriodIndex]
-  beta <- es$att.egt[-referencePeriodIndex]
+  hasReference <- any(es$egt == referencePeriod)
+  if ( hasReference ) {
+    referencePeriodIndex <- which(es$egt == referencePeriod)
+    V    <- V[-referencePeriodIndex,-referencePeriodIndex]
+    beta <- es$att.egt[-referencePeriodIndex]
+  } else {
+    beta <- es$att.egt
+  }
   
   nperiods <- nrow(V)
-  npre     <- sum(1*(es$egt < -1))
+  npre     <- sum(1*(es$egt < referencePeriod))
   npost    <- nperiods - npre
+  if ( !hasReference & (min(c(npost, npre)) <= 0) ) {
+    if ( npost <= 0 ) {
+      msg <- "not enough post-periods"
+    } else {
+      msg <- "not enough pre-periods"
+    }
+    msg <- paste0(msg, " (check your time vector; note honest_did takes -1 as the reference period)")
+    stop(msg)
+  }
+  
   baseVec1 <- basisVector(index=(e+1),size=npost)
   orig_ci  <- constructOriginalCS(betahat        = beta,
                                   sigma          = V,
@@ -79,122 +112,4 @@ honest_did.AGGTEobj <- function(es,
   }
   
   return(list(robust_ci=robust_ci, orig_ci=orig_ci, type=type))
-}
-
-# -----------------
-
-# Neu, von https://github.com/bcallaway11/did/blob/master/R/honest_did/honest_did.R
-
-#' @title honest_did
-#'
-#' @description a function to compute a sensitivity analysis
-#'  using the approach of Rambachan and Roth (2021)
-#' @param es an event study
-#' @export
-honest_did_new <- function(es, ...) {
-  UseMethod("honest_did_new", es)
-}
-
-
-#' @title honest_did.AGGTEobj
-#'
-#' @description a function to compute a sensitivity analysis
-#'  using the approach of Rambachan and Roth (2021) when
-#'  the event study is estimating using the `did` package
-#'
-#' @param e event time to compute the sensitivity analysis for.
-#'  The default value is `e=0` corresponding to the "on impact"
-#'  effect of participating in the treatment.
-#' @param type Options are "smoothness" (which conducts a
-#'  sensitivity analysis allowing for violations of linear trends
-#'  in pre-treatment periods) or "relative_magnitude" (which
-#'  conducts a sensitivity analysis based on the relative magnitudes
-#'  of deviations from parallel trends in pre-treatment periods).
-#' @inheritParams HonestDiD::createSensitivityResults
-#' @inheritParams HonestDiD::createSensitivityResults_relativeMagnitudes
-#' 
-#' @export
-honest_did_new.AGGTEobj <- function(es,
-                                e=0,
-                                type=c("smoothness", "relative_magnitude"),
-                                method=NULL,
-                                bound="deviation from parallel trends",
-                                Mvec=NULL,
-                                Mbarvec=NULL,
-                                monotonicityDirection=NULL,
-                                biasDirection=NULL,
-                                alpha=0.05,
-                                parallel=FALSE,
-                                gridPoints=10^3,
-                                grid.ub=NA,
-                                grid.lb=NA,
-                                ...) {
-  
-  
-  type <- type[1]
-  
-  # make sure that user is passing in an event study
-  if (es$type != "dynamic") {
-    stop("need to pass in an event study")
-  }
-  
-  # check if used universal base period and warn otherwise
-  if (es$DIDparams$base_period != "universal") {
-    warning("it is recommended to use a universal base period for honest_did")
-  }
-  
-  # recover influence function for event study estimates
-  es_inf_func <- es$inf.function$dynamic.inf.func.e
-  
-  # recover variance-covariance matrix
-  n <- nrow(es_inf_func)
-  V <- t(es_inf_func) %*% es_inf_func / (n*n) 
-  
-  #Remove the coefficient normalized to zero
-  referencePeriodIndex <- which(es$egt == -1)
-  V <- V[-referencePeriodIndex,-referencePeriodIndex]
-  beta <- es$att.egt[-referencePeriodIndex]
-  
-  nperiods <- nrow(V)
-  npre <- sum(1*(es$egt < 0))
-  npost <- nperiods - npre
-  
-  baseVec1 <- basisVector(index=(e+1),size=npost)
-  
-  orig_ci <- constructOriginalCS(betahat = es$att.egt,
-                                 sigma = V, numPrePeriods = npre,
-                                 numPostPeriods = npost,
-                                 l_vec = baseVec1)
-  
-  if (type=="relative_magnitude") {
-    if (is.null(method)) method <- "C-LF"
-    robust_ci <- createSensitivityResults_relativeMagnitudes(betahat = es$att.egt, sigma = V, 
-                                                             numPrePeriods = npre, 
-                                                             numPostPeriods = npost,
-                                                             bound=bound,
-                                                             method=method,
-                                                             l_vec = baseVec1,
-                                                             Mbarvec = Mbarvec,
-                                                             monotonicityDirection=monotonicityDirection,
-                                                             biasDirection=biasDirection,
-                                                             alpha=alpha,
-                                                             gridPoints=gridPoints,
-                                                             grid.lb=grid.lb,
-                                                             grid.ub=grid.ub,
-                                                             parallel=parallel)
-    
-  } else if (type=="smoothness") {
-    robust_ci <- createSensitivityResults(betahat = es$att.egt,
-                                          sigma = V, 
-                                          numPrePeriods = npre, 
-                                          numPostPeriods = npost,
-                                          method=method,
-                                          l_vec = baseVec1,
-                                          monotonicityDirection=monotonicityDirection,
-                                          biasDirection=biasDirection,
-                                          alpha=alpha,
-                                          parallel=parallel)
-  }
-  
-  list(robust_ci=robust_ci, orig_ci=orig_ci, type=type)
 }
